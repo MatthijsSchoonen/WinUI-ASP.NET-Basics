e237dd2ef32af3c3a858abf77a4518ec86f8899e
matthijs <113593159+MatthijsSchoonen@users.noreply.github.com>
2025-04-04 07:38:58 +0200

    file structure + user and role class

---
+++ WinUI-ASP.NET-Basics/WinUI-ASP.NET-Basics/Models/Role.cs
@@ -0,0 +1,8 @@
+﻿namespace WinUI_ASP.NET_Basics.Models
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; } 
+    }
+}
---
+++ WinUI-ASP.NET-Basics/WinUI-ASP.NET-Basics/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace WinUI_ASP.NET_Basics.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        public int RoleId { get; set; }
+        public Role? Role { get; set; }
+    }
+}
